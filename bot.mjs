b7663c75f296b667ea6dfa0914d3190b2f7b65d9
YOomDev <ype.oomes@gmail.com>
2024-12-16 17:32:59 +0100

    More implementation, getting close to being testable

--- bot.mjs
+++ bot.mjs
@@ -1,79 +1,105 @@
-import fs from 'fs';
+import fs from 'node:fs';
+import path from 'node:path';
 const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
 
-import { logError, logWarning, logInfo, sleep, contains } from "./utils.mjs";
+import { logError, logWarning, logInfo, sleep, contains, equals, randomInt, concat, readFile } from "./utils.mjs";
 
 // Bot file
 const commandProperties = ["name", "reply"];
 const config = loadJSON('./config.json');
 
+const commandList = [];
+const twitchChatters = [];
+
+let ready = false;
+const prefix = "!";
+
 export async function start(cmdProperties = []) {
     for (const properties of cmdProperties) { if (!contains(commandProperties, properties)) { commandProperties.push(properties); } }
-    client.login(config.token).catch(err => logError(err));
+    ready = false;
+    client.connect().catch(err => { logError(err); }).then(_ => { ready = true; });
+    while (!ready) { await sleep(0.25); }
+    ready = false;
     reload();
 }
 
 function reload() { registerCommands(); }
 
-
 ////////////////
-// Twitch bot //
+// Twitch Bot //
 ////////////////
 
-import path from 'node:path';
-import tmi from 'tmi.js';
-import { REST, Routes, Client, Collection, Events, GatewayIntentBits, EmbedBuilder, ActivityType } from 'discord.js';
+// Roles
+const DEVELOPER   = "Dev"
+const BROADCASTER = "Broadcaster";
+const MODERATOR   = "Moderator";
+const VIP         = "VIP";
+const SUBSCRIBER  = "Subscriber";
+const PRIME       = "Prime sub";
+const VIEWER      = "Viewer";
 
-// client
-const rest = new REST().setToken(config.token);
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection();
+const adminLevels = [
+    VIEWER,
+    PRIME,
+    SUBSCRIBER,
+    VIP,
+    MODERATOR,
+    BROADCASTER,
+    DEVELOPER
+];
 
-client.once(Events.ClientReady, readyClient => { logInfo(`Discord bot is ready! Logged in as ${readyClient.user.tag}`); });
+function getUserType(userState) {
+    if (equals(userState.username, superuserName)) { return DEVELOPER; }
+    if (userState.badges) { if (userState.badges['broadcaster']) { return BROADCASTER; } }
+    if (userState.mod) { return MODERATOR  ; }
+    if (userState.badges) { if (userState.badges['vip']) { return VIP; } }
+    if (userState.subscriber) { return SUBSCRIBER ; }
+    if (userState.badges) { if (userState.badges['premium']) { return PRIME; } }
+    logWarning("No role determined from:");
+    logData(userState.badges);
+    return VIEWER;
+}
 
-client.on(Events.MessageCreate, async message => {
-    if (message.author.id === client.user.id) { return; }
+function getAdminLevel(type) {
+    for (let i = 0; i < adminLevels.length; ++i) { if (type === adminLevels[i]) { return i; } }
+    logWarning(`No admin level found for type: ${type}`);
+    return -1;
+}
 
-    // TODO: handle messages
+const client = new tmi.Client({
+    options: { debug: true },
+    connection: { reconnect: true, secure: true },
+    identity: {
+        username: config.clientName,
+        password: `oauth:${config.token}`
+    },
+    channels: [`#${config.channel}`]
 });
-
-// Command handler
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        logWarning(`No command matching ${interaction.commandName} was found.`);
-        return;
+client.on('message', (channel, userState, message, self) => {
+    if (self) { return; }
+    if (containsIgnoreCase(config.ignoreUsers, userState['display-name'].toString())) {
+        // check if works
     }
 
-    try { await command.execute(interaction); }
-    catch (error) {
-        logError(error);
-        if (interaction.replied || interaction.deferred) { await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true }); }
-        else { await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }); }
-    }
+    // line below will be replaced by if statement above this when it works
+    for (let i = 0; i < ignoreUsers.length; i++) { if (equals(ignoreUsers[i].toLowerCase(), userState['display-name'].toString().toLowerCase())) { return; } }
+
+    parseTwitch(channel, userState, message).catch(err => logError(err));
 });
 
-// Make logging functions available for all the commands in other folders
-client.utils = {};
-client.utils.resolver = {
-    createRequest: createRequest,
-    resolveRequest: resolveRequest,
-    getSolvedRequest: getSolvedRequest
-};
 
-async function registerCommands() {
+
+function registerCommands() {
     logInfo("Started loading commands.");
-    client.commands.clear();
-    const commands = [];
-    const folders = ["./commands", "./twitch-bot-base/commands"];
-    // for (const folder of config.commandFolders) { folders.push(folder); }
+    commandList.slice(0, commandList.length);
+
+    const folders = ["./commands"];
+    for (const folder of commandFolders) { folders.push(folder); }
     for (const folder of folders) {
-        const commandFiles = fs.readdirSync(folder).filter(file => file.endsWith('.mjs'));
+        const commandFiles = fs.readdirSync(folder).filter(file => file.endsWith('.js'));
         for (const file of commandFiles) {
-            const filePath = "..\\" + path.join(folder, file);
-            let command = (await import(new URL(filePath, import.meta.url)).catch(err => logError(err)).then(_ => { return _; })).default;
+            const filePath = path.join(folder, file);
+            const command = require(filePath);
 
             // Check if command has all the needed properties
             let failed = false;
@@ -86,9 +112,135 @@ async function registerCommands() {
             if (failed) { continue; } // Skip
 
             // Set a new item in the Collection with the key as the command name and the value as the exported module
-            logInfo(`Loaded command '${command.data.name}'`);
-            client.commands.set(command.name, command);
-            commands.push(command.data.toJSON());
+            commandList.push({ name: command.data.name, command: command });
         }
+    }
+}
+
+async function parseTwitch(channel, userState, message) {
+    if (message.startsWith(prefix)) {
+        const params = message.trim().substring(prefix.length, message.length).split(" ");
+        const commandName = params[0].toLowerCase();
+        params.splice(0, 1);
+
+        const userId = userState['user-id'];
+        const adminLevel = getAdminLevel(getUserType(userState));
+
+        let found = false;
+        for (let i = 0; i < commandList.length; i++) {
+            if (equals(commandName, commandList[i].name)) {
+                const command = commandList[i];
+
+                // end
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+
+        }
+    } else {
+        if (!contains(twitchChatters, userId)) {
+            if (message.toString().indexOf("***") > -1) { return; }
+            if (hasURL(message)) { return; }
+            twitchChatters.push(userId);
+            const lines = readFile(`${config.automatedMessagesFolder}welcomeMessages${userState['first-msg'] ? "First" : ""}.txt`);
+            sendMessageTwitch(channel, lines[randomInt(lines.length)].replaceAll("{USER}", userState['display-name']));
+        }
+        messagesSinceLastAutomatedMessage++;
+    }
+}
+
+////////////////////////
+// Automated messages // // TODO: REFACTOR or REWRITE!
+////////////////////////
+
+let messagesNeededBeforeAutomatedMessage = 10;
+let messagesSinceLastAutomatedMessage = 0;
+let automatedMessageManager;
+let currentAutomatedMessage = 0;
+let runMessages = true;
+let hasTimePassedSinceLastAutomatedMessage = true;
+let automatedMessages = [];
+
+async function reloadTwitchTimedMessages() {
+    const messageConfig = readFile(`${config.automatedMessagesFolder}config.txt`);
+    for (let i = 0; i < messageConfig.length; i++) {
+        let line = messageConfig[i].split(" ");
+        switch (line[0]) {
+            case "message":
+            case "sequence":
+            case "list":
+                if (line.length > 1) {
+                    automatedMessages.push({ type: line[0], file: concat(line, " ", "", 1) });
+                    break;
+                }
+                logError(`Couldn\'t interpret automated message from config line ${i}: ${line}`);
+                break;
+            default:
+                logError(`Couldn\'t interpret automated message from config line ${i}: ${line}`);
+                break;
+        }
+    }
+    runMessages = false;
+    await stopAutomatedMessagesManager();
+    automatedMessageManager = automatedMessagesManager(); // Start new messages manager
+}
+
+async function stopAutomatedMessagesManager() {
+    runMessages = false;
+    if (automatedMessageManager) { await automatedMessageManager; }
+    automatedMessageManager = 0;
+    currentAutomatedMessage = 0;
+}
+
+function isChatActive() {
+    if (messagesSinceLastAutomatedMessage < messagesNeededBeforeAutomatedMessage) { return false; }
+    return hasTimePassedSinceLastAutomatedMessage;
+}
+
+async function awaitAutomatedMessageActive() { while (!isChatActive() && runMessages) { await sleep(1); } }
+
+async function automatedMessagesManager() {
+    runMessages = true;
+    while (runMessages) {
+        await awaitAutomatedMessageActive();
+        await playAutomatedMessage();
+    }
+}
+
+async function playAutomatedMessage() {
+    if (!runMessages) { return; }
+    if (isChatActive()) {
+        if (currentAutomatedMessage >= automatedMessages.length) { currentAutomatedMessage -= automatedMessages.length; }
+        const message = automatedMessages[currentAutomatedMessage];
+        let lines = readFile(`${config.automatedMessagesFolder}${message.file}.txt`);
+        switch (message.type) {
+            case "message":
+                sendMessageTwitch(config.twitchChannel, lines[randomInt(lines.length)]);
+                break;
+            case "sequence":
+                for (let i = 0; i < lines.length; i++) {
+                    await awaitAutomatedMessageActive();
+                    hasTimePassedSinceLastAutomatedMessage = false;
+                    messagesSinceLastAutomatedMessage = 0;
+                    sendMessageTwitch(config.twitchChannel, lines[i]);
+                    if (i < lines.length - 1) { sleep(minutesBetweenAutomatedMessages * 60).then(_ => { hasTimePassedSinceLastAutomatedMessage = true; }); }
+                }
+                break;
+            case "list":
+                for (let i = 0; i < lines.length; i++) {
+                    sendMessageTwitch(config.twitchChannel, lines[i]);
+                    if (i < lines.length - 1) {await sleep(5); }
+                }
+                break;
+            default:
+                logError(`Message type (${message.type}) not implemented. `);
+                break;
+        }
+        currentAutomatedMessage++;
+        hasTimePassedSinceLastAutomatedMessage = false;
+        messagesSinceLastAutomatedMessage = 0;
+        sleep(minutesBetweenAutomatedMessages * 60).then(_ => { hasTimePassedSinceLastAutomatedMessage = true; });
     }
 }
