525088a23ce13c1465ca1bd0bc95e4cc3d6e2e7b
YOomDev <ype.oomes@gmail.com>
2025-02-21 13:28:44 +0100

    Added the command functionality

--- commands/help.mjs
+++ commands/help.mjs
@@ -1,6 +1,9 @@
+import { concat } from "../utils.mjs";
+
 export default {
     name: "help",
     async reply(client, channel, userState, params, message) {
-
+        const commandList = concat(client.commands, ", ");
+        client.utils.sendChannelMessage(channel `Possible commands: ${commandList.toLowerCase()}`)
     }
 }
