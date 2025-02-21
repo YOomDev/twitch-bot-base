a2cfa31fdcaf0772aebac2cec493e3feadf99de4
YOomDev <ype.oomes@gmail.com>
2025-02-21 13:30:47 +0100

    Fixed function call arguments not being separated

--- commands/help.mjs
+++ commands/help.mjs
@@ -4,6 +4,6 @@ export default {
     name: "help",
     async reply(client, channel, userState, params, message) {
         const commandList = concat(client.commands, ", ");
-        client.utils.sendChannelMessage(channel `Possible commands: ${commandList.toLowerCase()}`)
+        client.utils.sendChannelMessage(channel, `Possible commands: ${commandList.toLowerCase()}`)
     }
 }
