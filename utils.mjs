77911f66be02069a1b86497001deb0accacd2f5b
YOomDev <ype.oomes@gmail.com>
2025-02-27 13:38:35 +0100

    Update log functions to show what bot they are from

--- utils.mjs
+++ utils.mjs
@@ -3,9 +3,9 @@ import fs from "node:fs";
 export function getTimeString(date = new Date()) { return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.toLocaleTimeString()}`.toString(); }
 
 // Log functions
-export function logError(err)   { console.error(`[${getTimeString()}] ERROR:\t`, err ); }
-export function logWarning(err) { console.error(`[${getTimeString()}] Warning:`, err ); }
-export function logInfo(info)   { console.log  (`[${getTimeString()}] Info:\t` , info); }
+export function logError(err)   { console.error(`[${getTimeString()}] Twitch ERROR:\t`, err ); }
+export function logWarning(err) { console.error(`[${getTimeString()}] Twitch Warning:`, err ); }
+export function logInfo(info)   { console.log  (`[${getTimeString()}] Twitch Info:\t` , info); }
 export function logData(data)   { console.log  (data); }
 export async function sleep(seconds) { return new Promise(resolve => setTimeout(resolve, Math.max(seconds, 0) * 1000)); }
 
