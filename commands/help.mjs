5182d6ab7c621581a67448e29647a2dace4a2c3a
YOomDev <ype.oomes@gmail.com>
2025-03-07 08:39:00 +0100

    Fix help command not listing the command list correctly

--- commands/help.mjs
+++ commands/help.mjs
@@ -3,7 +3,8 @@ import { concat } from "../utils.mjs";
 export default {
     name: "help",
     async reply(client, channel, userState, params, message) {
-        const commandList = concat(client.commands, ", ");
-        client.utils.sendChannelMessage(channel, `Possible commands: ${commandList.toLowerCase()}`)
+        let commandList = [];
+        for (let i = 0; i < client.commands.length; i++) { commandList.push(client.commands[i].name); }
+        client.utils.sendChannelMessage(channel, `Possible commands: ${concat(commandList, ", ").toLowerCase()}`)
     }
 }
