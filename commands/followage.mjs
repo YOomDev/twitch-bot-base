ce680fd33ad9ae505a773ccb64606ce247e6d6d8
YOomDev <ype.oomes@gmail.com>
2025-03-17 17:00:55 +0100

    Fix wrong usage of utils function which caused a crash by calling a non-existing function

--- commands/followage.mjs
+++ commands/followage.mjs
@@ -2,7 +2,7 @@ export default {
     // Twitch
     name: "followage",
     async reply(client, channel, userState, params, message) {
-        const follower = client.isFollower(userState['user-id']);
+        const follower = client.utils.isFollower(userState['user-id']);
         client.utils.sendChannelMessage(channel, follower < 0 ? "You have not followed long enough to check" : getTimeDifferenceInDays(followerData[follower].time));
     },
 };
