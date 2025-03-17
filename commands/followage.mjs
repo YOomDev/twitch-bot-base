ecc441a26626789d9c9423b3e0d06bd1fddda015
YOomDev <ype.oomes@gmail.com>
2025-03-17 16:34:05 +0100

    Add followage command

---
+++ commands/followage.mjs
@@ -0,0 +1,19 @@
+export default {
+    // Twitch
+    name: "followage",
+    async reply(client, channel, userState, params, message) {
+        const follower = client.isFollower(userState['user-id']);
+        client.utils.sendChannelMessage(channel, follower < 0 ? "You have not followed long enough to check" : getTimeDifferenceInDays(followerData[follower].time));
+    },
+};
+
+function getTimeDifferenceInDays(milliFrom, milliTo = new Date().getTime(), showMinutes = false) {
+    const totalMinutes = Math.floor((milliTo - milliFrom) / 1000 / 60);
+    const totalHours = Math.floor(totalMinutes / 60);
+    const totalDays = Math.floor(totalHours / 24);
+    const years = Math.floor(totalDays / 365);
+    const days = totalDays - (years * 365);
+    const hours = totalHours - (totalDays * 24);
+    const minutes = totalMinutes - (totalHours * 60);
+    return `${years > 0 ? `${years} years and ` : ``}${days > 0 ? `${days} days and ` : ``}${hours} hours${showMinutes ? (minutes > 0 ? ` and ${minutes} minutes` : ``) : ``}`;
+}
