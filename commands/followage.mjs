2f51091349a88704dee716dbff44e4ff191dc16f
YOomDev <ype.oomes@gmail.com>
2025-03-18 20:01:32 +0100

    Fix followage not using index and causing undefined behavior, moved getTimeDifference to utils function in client

--- commands/followage.mjs
+++ commands/followage.mjs
@@ -3,17 +3,6 @@ export default {
     name: "followage",
     async reply(client, channel, userState, params, message) {
         const follower = client.utils.isFollower(userState['user-id']);
-        client.utils.sendChannelMessage(channel, follower < 0 ? "You have not followed long enough to check" : getTimeDifferenceInDays(client.utils.getFollowerTime()));
+        client.utils.sendChannelMessage(channel, follower < 0 ? "You have not followed long enough to check" : client.utils.getTimeDifference(client.utils.getFollowerTime(follower)));
     },
 };
-
-function getTimeDifferenceInDays(milliFrom, milliTo = new Date().getTime(), showMinutes = false) {
-    const totalMinutes = Math.floor((milliTo - milliFrom) / 1000 / 60);
-    const totalHours = Math.floor(totalMinutes / 60);
-    const totalDays = Math.floor(totalHours / 24);
-    const years = Math.floor(totalDays / 365);
-    const days = totalDays - (years * 365);
-    const hours = totalHours - (totalDays * 24);
-    const minutes = totalMinutes - (totalHours * 60);
-    return `${years > 0 ? `${years} years and ` : ``}${days > 0 ? `${days} days and ` : ``}${hours} hours${showMinutes ? (minutes > 0 ? ` and ${minutes} minutes` : ``) : ``}`;
-}
