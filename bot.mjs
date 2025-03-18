c6edf92d7b0c422803da45cabbc1bced71824fff
YOomDev <ype.oomes@gmail.com>
2025-03-18 18:02:16 +0100

    Added standardized replies in client utils for command permission checks

--- bot.mjs
+++ bot.mjs
@@ -99,6 +99,12 @@ client.utils.getFollowerTime = function (index) {
     return followerData[index].time;
 }
 client.utils.isAdminLevel = function (userState, role) { return getAdminLevel(getUserType(userState)) >= getAdminLevel(role); }
+client.replies = {};
+client.replies.BRC_NEEDED = "yo udo not have the correct permission for this command, you need to be at least a Broadcaster to use this";
+client.replies.MOD_NEEDED = "yo udo not have the correct permission for this command, you need to be at least a Moderator to use this";
+client.replies.SUB_NEEDED = "yo udo not have the correct permission for this command, you need to be at least a Subscriber to use this";
+client.replies.PRI_NEEDED = "yo udo not have the correct permission for this command, you need to be at least a Prime Subscriber to use this";
+client.replies.FOL_NEEDED = "yo udo not have the correct permission for this command, you need to be at least a Follower to use this";
 client.roles = {};
 client.roles.DEVELOPER   = DEVELOPER;
 client.roles.BROADCASTER = BROADCASTER;
