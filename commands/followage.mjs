230c58b07ace1966b227b2c2d0b96483441ee6a7
YOomDev <ype.oomes@gmail.com>
2025-04-07 12:12:59 +0200

    Moved followage command to command folder

--- commands/followage.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-    // Twitch
-    name: "followage",
-    async reply(client, channel, userState, params, message) {
-        const follower = client.utils.isFollower(userState['user-id']);
-        client.utils.sendChannelMessage(channel, follower < 0 ? "You have not followed long enough to check" : client.utils.getTimeDifference(client.utils.getFollowerTime(follower)));
-    },
-};
