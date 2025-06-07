430db03731d994d43288120d6b898500da19e923
YOomDev <ype.oomes@gmail.com>
2025-06-07 15:27:55 +0200

    Added support to make commands hidden

--- commands/help.mjs
+++ commands/help.mjs
@@ -4,7 +4,7 @@ export default {
     name: "help",
     async reply(client, channel, userState, params, message) {
         let commandList = [];
-        for (let i = 0; i < client.commands.length; i++) { commandList.push(client.commands[i].name); }
+        for (let i = 0; i < client.commands.length; i++) { if (client.commands[i].hidden === false || !("hidden" in client.commands[i])) { commandList.push(client.commands[i].name); } }
         client.utils.sendChannelMessage(channel, `Possible commands: ${concat(commandList, ", ", "!").toLowerCase()}`)
     }
 }
